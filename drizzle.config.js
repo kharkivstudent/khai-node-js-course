acbd2efb8e38bec2036b9058d380a032ec843392
Viktoriia Hlushchenko <heejaeja403@gmail.com>
2024-05-04 22:22:52 +0200

    database setup

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+export default {
+    schema: 'C:/Users/lmntr/Desktop/webarch/khai-node-js-course/scr/db/schema.js',
+    out: './drizzle',
+    driver: 'pg',
+    dbCredentials: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'nodejs_course_admin',
+        password: 'my_password',
+        database: 'nodejs_course_database',
+    },
+};
